7e009219fbb167f3428b4887d996e73df498a617
PawanKL <34525906+PawanKL@users.noreply.github.com>
2020-06-18 14:04:15 +0500

    add ERP apis

---
+++ IptApis/Shared/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IptApis.Shared
+{
+    public class Test
+    {
+        object data;
+        object tablename; 
+    }
+}
